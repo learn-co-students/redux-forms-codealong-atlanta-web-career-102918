bf1ca8959259c75b6a8e47ff5cf60c5b32e3d5ca
Andrew Jones <joneseyit@protonmail.com>
2019-04-17 16:17:06 -0400

    setup reducer

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,13 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-
-  console.log("reducer received this action:", action);
+  switch(action.type){
+    case "ADD_TODO":
+      return { todos: state.todos.concat(action.paylod.text) };
+    default: 
+      return state
+  }
+ 
 
   return state;
 }
